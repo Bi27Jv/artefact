be80682a1ae6dfac182529fc0c5f9b193a6a50a2
Bi27Jv <bi27jv@student.sunderland.ac.uk>
2026-03-10 17:37:32 +0000

    Created 'ApplicationDbContext.cs' file.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using artefact.Models;
+
+namespace artefact.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // Define DbSet for models below:
+        public DbSet<User> Users { get; set; }
+
+        // Context below prevents duplicate email adresses in the Users table:
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+        }
+    }
+}
