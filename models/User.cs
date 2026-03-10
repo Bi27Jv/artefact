a7dc9f52b8961c718d9760cd97d28a27ca7c6bca
Bi27Jv <bi27jv@student.sunderland.ac.uk>
2026-03-10 17:19:43 +0000

    added user model.

--- models/User.cs
+++ models/User.cs
@@ -2,5 +2,11 @@
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordEncrypted { get; set; }
+
+
     }
 }
