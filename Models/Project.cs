301626a1fe703d91c8f35445ed63bc68463c8d26
Bi27Jv <Bi27Jv@student.sunderland.ac.uk>
2026-05-06 16:43:56 +0100

    Index nearly complete, displays both messages when logged in.

--- Models/Project.cs
+++ Models/Project.cs
@@ -4,14 +4,14 @@
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
-        public string Description { get; set; }
+        public string Description { get; set; } = string.Empty;
 
         public DateTime CreatedDate { get; set; } = DateTime.Now;
 
         // Foreign key to User, connects a project to its creator
         public int UserId { get; set; }
-         public User User { get; set; }
+        public User User { get; set; } = null!;
     }
 }
