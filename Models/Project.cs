30107fbe435d842129891235104031301793f124
Bi27Jv <Bi27Jv@student.sunderland.ac.uk>
2026-05-07 13:27:42 +0100

    Project creation functions but doesn't look good. Bug with index page, no manage page yet so it doesn't display anything.

--- Models/Project.cs
+++ Models/Project.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
 
-        public string Name { get; set; } = string.Empty;
+        public string Name { get; set; }
 
-        public string Description { get; set; } = string.Empty;
+        public string Description { get; set; }
 
         public DateTime CreatedDate { get; set; } = DateTime.Now;
 
--- Pages/Projects/Create.cshtml.cs
+++ Pages/Projects/Create.cshtml.cs
@@ -1,12 +1,47 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using artefact.Data;
+using artefact.Models;
 
 namespace artefact.Pages.Projects
 {
+    [Authorize] // Makes sure that this page can only be accessed if the user is logged in
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+        public CreateModel(ApplicationDbContext db)
         {
+            _db = db;
         }
+
+        [BindProperty]
+        public string Name { get; set; } = string.Empty;
+
+        [BindProperty]
+        public string Description { get; set; } = string.Empty;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+            var project = new Project
+            {
+                Name = Name,
+                Description = Description,
+                UserId = userId,
+                CreatedDate = DateTime.UtcNow
+            };
+
+            _db.Projects.Add(project);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("/Index");
+        }
+        
     }
 }
